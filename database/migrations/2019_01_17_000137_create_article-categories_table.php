3eb3238d882a595def625f44eab6805d856671bb
DavidOmel <progdavomel@gmail.com>
2019-01-18 00:48:21 +0200

    Migrations is ready

--- database/migrations/2019_01_17_000137_create_article-categories_table.php
+++ database/migrations/2019_01_17_000137_create_article-categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateArticleCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('article-categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('email', 128)->unique();
-            $table->string('password');
-            $table->boolean('isadmin')->default(false);
-            $table->rememberToken();
+            $table->integer('category_id');
+            $table->integer('article_id');
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('article-categories');
     }
 }
--- database/migrations/2019_01_17_223844_create_comments_table.php
+++ database/migrations/2019_01_17_223844_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('email', 128)->unique();
-            $table->string('password');
-            $table->boolean('isadmin')->default(false);
-            $table->rememberToken();
+            $table->integer('article_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->boolean('status')->default(false);
+            $table->text('comments')->nullable();
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments');
     }
 }
